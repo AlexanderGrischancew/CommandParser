90007e61fa99daf8333fa57dffa7ca82249d91d2
Alexander <alexander.grischancew@yahoo.de>
2017-11-22 12:29:28 +0100

    Add files via upload

---
+++ CommandExample.java
@@ -0,0 +1,59 @@
+import Commands.CommandLibrary;
+import Commands.ReturnPackage;
+
+public class CommandExample {
+
+    private static boolean run = true;
+    private static CommandLibrary commandLibrary = new CommandLibrary();
+
+    public static void main(String args[]){
+        setup();
+        while(run){
+
+            //READ INPUT
+            String input = "quit";
+            //----------
+
+            printReturnPackage(commandLibrary.executeCommand(input));
+        }
+    }
+
+    private static void setup(){
+        //SETUP
+        //ADD COMMANDS TO LIBARY
+        for(CommandsEnum commandEnumCommand: CommandsEnum.values()){
+            commandLibrary.addCommand(commandEnumCommand.getCommandRegex(),commandEnumCommand.getCommand());
+        }
+    }
+
+    //Halts the main loop
+    private static void stop(){
+        run = false;
+    }
+
+
+    //This method grants that only the main class can print to the user.
+    private static void printReturnPackage(ReturnPackage returnPackage){
+        // As every new-line needs to be outputed separately, the toString of
+        // the Message is split at "\n" and then a Terminal.printline
+        // method-call is conducted for each token.
+        String[] messageLines = returnPackage.getMessage().trim().split("\n");
+
+        if (returnPackage.isError() && !returnPackage.getMessage().equals("")) {
+            for (String line : messageLines) {
+                Terminal.printError(line.trim());
+            }
+        } else if (!returnPackage.isError() && !returnPackage.getMessage().equals("")) {
+            for (String line : messageLines) {
+                Terminal.printLine(line.trim());
+            }
+        }
+
+        //If the ReturnMessage carries a Exit Signal:
+        if (returnPackage.isExitFlag()){
+            stop();
+            return;
+        }
+    }
+
+}
---
+++ CommandsEnum.java
@@ -0,0 +1,54 @@
+import Commands.Command;
+import Commands.ExitCommand;
+
+import java.util.Set;
+
+public enum CommandsEnum {
+
+    QUIT_COMMAND("quit","",new ExitCommand()),
+    ADD_COMMAND("add","",null),
+    GOAL_COMMAND("goal","",null),
+    REMOVE_GOAL_COMMAND("remove-goal","",null),
+    GOALS_COMMAND("goals","",null),
+    RECORD_COMMAND("record","",null),
+    STATE_COMMAND("state","",null),
+    PROGRESS_COMMAND("progress","",null),
+    TOP_COMMAND("top","",null),
+    GOALS_WEEK_COMMAND("goals-week","",null),
+    NEXT_DAY_COMMAND("next-day","",null),
+    SET_DAY_COMMAND("set-day","[0-9]+",null),
+    //---
+    SLIDING_WINDOW_COMMAND("sliding-window","",null),
+    NORMALIZE_COMMAND("normalize","",null),
+    PEAKS_COMMAND("peaks","",null),
+    PULSE_COMMAND("pulse","",null);
+
+    private final String commandPrefix;
+    private final String commandSuffix;
+    private final Command command;
+
+
+
+    CommandsEnum(String commandPrefix,String commandSuffix, Command command){
+        this.commandPrefix = commandPrefix;
+        this.commandSuffix = commandSuffix;
+        this.command = command;
+    }
+
+    public String getCommandPrefix() {
+        return commandPrefix;
+    }
+
+    public String getCommandSuffix() {
+        return commandSuffix;
+    }
+
+    public String getCommandRegex() {
+        return (commandPrefix+" "+commandSuffix).trim();
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+}
